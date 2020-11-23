767d52fbf66bc47326fc075eb970258fe7c8cf78
si-wu-physics <wusi1983@gmail.com>
2020-11-22 21:40:38 -0800

    added tests on more functions

--- virtual/virtual.cpp
+++ virtual/virtual.cpp
@@ -44,17 +44,27 @@ namespace
 int main(void)
 {
   {
+    std::cout << "Test the Base class virtual functions...\n";
+
     Base b(1000);
     long* vptrAddress = (long*)&b;
     std::cout << "The address of the virtual table is " << vptrAddress << "\n";
 
     typedef void (*func)(void);
-    func fptr = (func)*((long*)(*(long*)(&b)) + 1);
+    func fptr = (func)*((long*)(*(long*)(&b)));
+    std::cout << "Calling Base::print() by its address: ";
+    fptr();
+
+    fptr = (func)*((long*)(*(long*)(&b)) + 1);
     std::cout << "Calling Base::set() by its address: ";
     fptr();
+
+    std::cout << "\n";
   }
 
   {
+    std::cout << "Test the Derived class virtual functions...\n";
+
     Derived d(1000);
     long* vptrAddress = (long*)&d;
     std::cout << "The address of the virtual table is " << vptrAddress << "\n";
@@ -63,6 +73,12 @@ int main(void)
     func fptr = (func)*((long*)(*(long*)(&d)));
     std::cout << "Calling Derived::print() by its address: ";
     fptr();
+
+    fptr = (func)*((long*)(*(long*)(&d)) + 1);
+    std::cout << "Calling Derived::set() by its address: ";
+    fptr();
+
+    std::cout << "\n";
   }
 
   return 0;
